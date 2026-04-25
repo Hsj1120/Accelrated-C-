0ac5378d50f0f5eceeebb6036ac6ef48e83f7b7d
Soonjae Han <hsj11209@gmail.com>
2026-04-25 18:41:58 +0900

    Chapter 9.2.1

---
+++ CPP/Chapter 9/Chapter 9.cpp
@@ -0,0 +1,19 @@
+﻿#include <vector>
+#include <iostream>
+#include <string>
+using std::vector;
+using std::string;
+
+struct Student_info {
+	std::string name;
+	double midterm, final;
+	std::vector<double> homework;
+
+	std::istream& read(std::istream&);
+	double grade() const;
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
