c8c4bbef0eb34976e5a1f261144634455dd105b9
Soonjae Han <hsj11209@gmail.com>
2026-05-14 17:32:48 +0900

    Chapter 9.1.4

---
+++ CPP/Chapter 10/Chapter 10.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using std::cout; using std::endl;
+
+int next(int n)
+{
+	return n + 1;
+
+}
+
+int main()
+{
+	int i = 0;
+	int (*fp)(int) = next; // fp는 next 함수를 가리킴
+
+	fp = next; // fp는 next 함수를 가리킴
+
+	i = fp(i); 
+	cout << "i = " << i << endl;
+
+	i = (*fp)(i); 
+	// p를 통해 x의 값을 변경
+	cout << "i = " << i << endl;
+
+	return 0;
+}
