d3c3ba68b5df2f02ca4809a21d590c8d6eac46a6
Soonjae Han <hsj11209@gmail.com>
2026-05-17 16:51:17 +0900

    Chapter 10.4

--- CPP/Chapter 10/Chapter 10.cpp
+++ CPP/Chapter 10/Chapter 10.cpp
@@ -1,25 +1,34 @@
 ﻿#include <iostream>
-using std::cout; using std::endl;
+#include <string>
+using std::cout; using std::endl; using std::string;
 
-int next(int n)
+string letter_grade(double grade)
 {
-	return n + 1;
+	// 각 범위를 구분하는 점수
+	static const double numbers[] = { 97,94,90,87,84,80,77,74,70,60,0 };
 
+	// 각 범위에 대응하는 학점
+	static const char* const letters[] = { "A+","A","A-","B+","B","B-","C+","C","D+","D","F" };
+
+	// 배열 크기와 요소 하나의 크기를 사용하요 각 범위를 구분하는 점수 개수를 구함
+	static const size_t ngrades = sizeof(numbers) / sizeof(*numbers);
+
+	// 주어진 최종 점수에 해당하는 문자 등급을 찾아서 반환
+	for (size_t i = 0; i < ngrades; ++i) {
+		if (grade >= numbers[i])
+			return letters[i];
+	}
+	return "?\?\?";
 }
 
-int main()
+int main(int argc, char** argv)
 {
-	int i = 0;
-	int (*fp)(int) = next; // fp는 next 함수를 가리킴
-
-	fp = next; // fp는 next 함수를 가리킴
-
-	i = fp(i); 
-	cout << "i = " << i << endl;
-
-	i = (*fp)(i); 
-	// p를 통해 x의 값을 변경
-	cout << "i = " << i << endl;
-
+	// 명령 프롬프트에 입력한 인수가 존재하면 출력
+	if (argc > 1) {
+		int i;
+		for (i = 1; i < argc-1; ++i)
+			cout << argv[i] << " ";
+		cout << argv[i] << endl;
+	}
 	return 0;
 }
